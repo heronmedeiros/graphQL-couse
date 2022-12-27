017e4f23cfdf7ce42153622c71d965ef5382c556
Heron Medeiros <heron.medeiros@gmail.com>
2022-12-27 13:24:51 -0300

    added UserType

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 'use strict';
 
 const express        = require('express');
-// const expressGraphQL = require('express-graphql');
-var { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
-
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
-
 
 app.listen(4000, () => {
     console.log('listening on 4000')
